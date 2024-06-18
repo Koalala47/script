5e7e75d296edc4ad4a0a2117c3ea2228d2bffb0c
Koalala47 <1092135739@qq.com>
2024-06-18 23:46:21 +0800

    Delete ves.js

--- ves.js
+++
@@ -1,7 +0,0 @@
-var response = JSON.parse($response.body);
-
-response.mbComboList.forEach(function(combo) {
-    combo.endTime = "2099-01-01 00:00:00";
-});
-
-$done({body: JSON.stringify(response)});
